892515901097da941e111abe2ab109e05e01701e
amy <ctrlalternativedelete@gmail.com>
2024-10-27 18:46:07 +1100

    wip

--- Lib/IAsChildParent.cs
+++ Lib/IAsChildParent.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components;
 
 namespace PetShop.Lib;
 
+/**
+ * Interface used to strongly type a Parent component that supports the AsChild pattern inspired by https://www.radix-ui.com/primitives/docs/guides/composition
+ */
 public interface IAsChildParent
 {
     public IDictionary<string, object>? AdditionalAttributes { get; set; }
--- Lib/Services/NavMenuStateService/INavMenuStateService.cs
+++ Lib/Services/NavMenuStateService/INavMenuStateService.cs
@@ -1,5 +1,8 @@
 namespace PetShop.Lib.Services.NavMenuStateService;
 
+/**
+ * Interface used to allow for mocked dependency injection
+ */
 public interface INavMenuStateService
 {
     public NavMenuState State { get; set; }
